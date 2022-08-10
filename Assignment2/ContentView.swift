e55103ec3114f26618a517cfadc1bd9b37b29280
nhat117 <s3878174@rmit.edu.vn>
2022-08-10 11:28:27 +0700

    Kick start project

--- Assignment2/ContentView.swift
+++ Assignment2/ContentView.swift
@@ -7,10 +7,24 @@
 
 import SwiftUI
 
+//Storing the grid of of the tictactoe game
+let cols = [GridItem(.flexible()), GridItem(.flexible()),GridItem(.flexible())]
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { screenSize in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: cols, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Rectangle().foregroundColor(Color(red: 0.56, green: 0.56, blue: 0.57)).opacity(0.65).frame(width: screenSize.size.width/4, height: screenSize.size.width/4).cornerRadius(20).padding(5)
+                            Image(systemName: "xmark").resizable().scaledToFit().frame(width: screenSize.size.width/5, height: screenSize.size.width/5)
+                        }
+                    }
+                }.padding([.trailing,.leading],20)
+                Spacer()
+            }
+        }
     }
 }
 
