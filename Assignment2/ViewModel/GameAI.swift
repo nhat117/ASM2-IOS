59ce87f0e378d61c5b2b278726c79d3b7db6d4f7
nhat117 <s3878174@rmit.edu.vn>
2022-08-19 20:52:01 +0700

    Feat: Menu View

---
+++ Assignment2/ViewModel/GameAI.swift
@@ -0,0 +1,8 @@
+//
+//  GameAI.swift
+//  Assignment2
+//
+//  Created by Nhat Bui Minh on 19/08/2022.
+//
+
+import Foundation
---
+++ Assignment2/ViewModel/GameEngineViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameEngineViewModel.swift
+//  Assignment2
+//
+//  Created by Nhat Bui Minh on 19/08/2022.
+//
+
+import Foundation
