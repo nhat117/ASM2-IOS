60854ef3c87409b2fff9e5bad2549fad69978a7c
nhat117 <s3878174@rmit.edu.vn>
2022-08-21 20:10:39 +0700

    ađ

--- Assignment2/ViewModel/LinkView.swift
+++ Assignment2/ViewModel/LinkView.swift
@@ -5,4 +5,25 @@
 //  Created by Nhat Bui Minh on 20/08/2022.
 //
 
-import Foundation
+import SwiftUI
+import AVKit
+import WebKit
+
+struct YoutubeVideoView: UIViewRepresentable {
+    
+    var youtubeVideoID: String
+    
+    func makeUIView(context: Context) -> WKWebView  {
+        
+        WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+        let path = "https://www.youtube.com/embed/\(youtubeVideoID)"
+        guard let url = URL(string: path) else { return }
+        
+        uiView.scrollView.isScrollEnabled = true
+        uiView.load(.init(url: url))
+    }
+}
